b28d11a64c7acbab0d380d8ab93dda6236497f6b
Keping <kw238@duke.edu>
2017-01-21 01:59:55 -0500

    use resource stream correctly

--- src/breakout/Level.java
+++ src/breakout/Level.java
@@ -1,8 +1,8 @@
 package breakout;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,9 +16,8 @@ public class Level {
 	
 	public Level(String fileName) {
 		try {
-			ClassLoader classLoader = getClass().getClassLoader();
-			File file = new File(classLoader.getResource(fileName).getFile());
-			BufferedReader br = new BufferedReader(new FileReader(file));
+			InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 			String line;
 			list = new ArrayList<int[][]>();
 			while ((line = br.readLine()) != null) {
